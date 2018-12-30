d19b0aa48ee5a4d07222302ef078e8af79000601
Alex_Beng <599267065@qq.com>
2018-12-30 16:46:57 +0800

    build structure

---
+++ ColorReconitor/include/Kmeans.h
@@ -0,0 +1,10 @@
+#ifndef KMEANS_H
+#define KMEANS_H
+
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#undef KMEANS_H
+#endif // KMEANS_H
---
+++ ColorReconitor/source/Kmeans.cpp
@@ -0,0 +1,3 @@
+#include "../include/Kmeans.h"
+
+
